b38d2656fb83fffd63e4fe0ba2fffb6cd5eeac25
[Herelli] <heatherelli@heathers-MacBook-Pro.local>
2014-07-31 10:11:40 -0400

    customize routes, update 'cities' views, generate helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+
+	# Here, we'll have a master list of answers,
+	# and the user's guess will be checked against this list.
+
+	def check_answer(answer)
+
+		citylist = ['Atlanta', 'Asheville', 'Charlotte', 'Raleigh']
+
+			#Additional master lists could be placed here.
+
+		correct = false
+
+		citylist.each do |c| ## look up block variables
+			if answer == c
+				correct = true
+			end
+		end	
+		return correct
+	end	
+
+	#check to see if the answer, while correct, may have already
+	#been guessed and put into the datebase of answers.
+
+	def check_for_dup(answer, database)
+
+		is_dup = false
+
+		database.each do |d|	
+		if answer == d.name
+			is_dup = true
+		end
+	end	
+	return is_dup
+
 end
